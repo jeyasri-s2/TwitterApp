4347c8a77be959310d0c9f82ab92163873a99772
Subarna Chowdhury Soma <subarna089@gmail.com>
2019-10-20 22:27:56 -0700

    Unit test- authentication check

--- TwitterApp/tweets/tests/test_views.py
+++ TwitterApp/tweets/tests/test_views.py
@@ -2,6 +2,21 @@ import json
 from rest_framework import status
 from django.test import TestCase, Client
 from django.urls import reverse
+import unittest
+from ..views import getAPIAuth
+
 
 # initialize the APIClient app
 client = Client()
+
+# to check authentication
+class BasicTests(unittest.TestCase):
+    def test_request_response(self):
+        response = getAPIAuth()
+
+        # Assert that the request-response cycle completed successfully with status code 200.
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
